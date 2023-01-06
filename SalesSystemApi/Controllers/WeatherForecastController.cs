e8822e6ecec06ee9703ba97f36fc9979be145cd6
zearkiatos <caprilespe@outlook.com>
2023-01-06 18:18:56 -0300

    Edit default endpoint

--- SalesSystemApi/Controllers/WeatherForecastController.cs
+++ SalesSystemApi/Controllers/WeatherForecastController.cs
@@ -6,11 +6,6 @@ namespace SalesSystemApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -21,12 +16,11 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        List<WeatherForecast> weatherForecasts = new List<WeatherForecast>();
+
+        weatherForecasts.Add(new WeatherForecast() { Id = 5, Name = "Hector" });
+        weatherForecasts.Add(new WeatherForecast() { Id = 6, Name = "Francisco" });
+
+        return weatherForecasts;
     }
 }
